167b39ecbf8181996ac007f46a87218e4074d693
Mikhail <michaildrazdovich@gmail.com>
2020-12-10 11:21:16 +0300

    Add eslint ignore

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import { useResize } from 'hooks';
 import ScreenContext from 'contexts/screen';
