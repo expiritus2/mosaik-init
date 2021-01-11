6010c3a5fc71f662ef5a9bbd9ab559cb863efb95
Mikhail <michaildrazdovich@gmail.com>
2021-01-11 23:18:03 +0300

    feat(components): improve tabs compontnt

--- src/components/Tabs/Content/index.js
+++ src/components/Tabs/Content/index.js
@@ -4,13 +4,16 @@ import classNames from 'classnames';
 
 import styles from './styles.module.scss';
 
-const Content = ({ isActive, children, animation }) => {
+const Content = ({ isActive, children, animation, direction }) => {
     if (!isActive) return null;
 
     return (
-        <div className={classNames(styles.content, styles[animation], {
-            [styles.active]: isActive,
-        })}
+        <div className={classNames(
+            styles.content,
+            styles[animation],
+            styles[direction],
+            { [styles.active]: isActive },
+        )}
         >
             {children}
         </div>
@@ -21,6 +24,7 @@ Content.propTypes = {
     isActive: PropTypes.bool,
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.object]).isRequired,
     animation: PropTypes.string.isRequired,
+    direction: PropTypes.string.isRequired,
 };
 
 Content.defaultProps = {
--- src/components/Tabs/Tab/index.js
+++ src/components/Tabs/Tab/index.js
@@ -4,14 +4,25 @@ import classNames from 'classnames';
 
 import styles from './styles.module.scss';
 
-const Tab = ({ label, onClick, isActive }) => (
-    <div className={classNames(styles.tab, { [styles.active]: isActive })} onClick={onClick} key={label}>{label}</div>
+const Tab = ({ label, onClick, isActive, direction }) => (
+    <div
+        key={label}
+        className={classNames(
+            styles.tab,
+            styles[direction],
+            { [styles.active]: isActive },
+        )}
+        onClick={onClick}
+    >
+        {label}
+    </div>
 );
 
 Tab.propTypes = {
     label: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
     isActive: PropTypes.bool,
+    direction: PropTypes.string.isRequired,
 };
 
 Tab.defaultProps = {
