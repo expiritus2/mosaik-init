c86ff9273ac49e5d9b45a4cd34e6abba75d0bbb4
Mikhail <michaildrazdovich@gmail.com>
2021-01-11 23:18:03 +0300

    feat(components): improve tabs compontnt

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 
 import { getQuery, setQuery } from 'helpers';
 import Tab from './Tab';
@@ -9,7 +10,7 @@ import Content from './Content';
 import styles from './styles.module.scss';
 
 const Tabs = (props) => {
-    const { tabs, animation, enableUrlParams, activeTabIndex, ...rest } = props;
+    const { tabs, animation, enableUrlParams, activeTabIndex, wrapperClassName, direction, ...rest } = props;
     const { location, history } = rest;
 
     const [activeTab, setActiveTab] = useState(enableUrlParams
@@ -37,18 +38,19 @@ const Tabs = (props) => {
             label={label}
             onClick={() => onClick(index)}
             isActive={getIsActiveTab(index)}
+            direction={direction}
         />
     ));
 
     const renderContent = () => tabs.map(({ label, Component }, index) => (
-        <Content key={label} animation={animation} isActive={getIsActiveTab(index)}>
+        <Content key={label} animation={animation} isActive={getIsActiveTab(index)} direction={direction}>
             <Component {...rest} />
         </Content>
     ));
 
     return (
-        <div>
-            <div className={styles.tabs}>{renderTabs()}</div>
+        <div className={classNames(styles.wrapper, styles[direction], wrapperClassName)}>
+            <div className={classNames(styles.tabs)}>{renderTabs()}</div>
             {renderContent()}
         </div>
     );
@@ -60,7 +62,11 @@ Tabs.SLIDE_IN_BOTTOM = 'slideInBottom';
 Tabs.SLIDE_IN_LEFT = 'slideInLeft';
 Tabs.SLIDE_IN_RIGHT = 'slideInRight';
 
+Tabs.DIRECTION_HORIZONTAL = 'horizontal';
+Tabs.DIRECTION_VERTICAL = 'vertical';
+
 Tabs.propTypes = {
+    wrapperClassName: PropTypes.string,
     tabs: PropTypes.arrayOf(PropTypes.shape({
         label: PropTypes.string,
         Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
@@ -68,12 +74,15 @@ Tabs.propTypes = {
     animation: PropTypes.string,
     enableUrlParams: PropTypes.bool,
     activeTabIndex: PropTypes.number,
+    direction: PropTypes.string,
 };
 
 Tabs.defaultProps = {
+    wrapperClassName: '',
     animation: Tabs.SLIDE_IN_RIGHT,
     enableUrlParams: false,
     activeTabIndex: 0,
+    direction: Tabs.DIRECTION_HORIZONTAL,
 };
 
 export default withRouter(Tabs);
